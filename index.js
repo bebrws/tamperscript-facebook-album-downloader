920bec3b0d0e91b6b80f26a25902b35a02ce7384
Brad Barrows <bebrws@gmail.com>
2021-06-05 08:34:05 -0700

    before try except

---
+++ index.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Download all images in a Facebook Album
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @grant        none
+// @match        https://www.facebook.com/*/photos/*
+// @match        https://www.facebook.com/photo.php?*
+// @match        https://www.facebook.com/media/set/*
+// @grant        GM_download
+// @description  try to take over the world!
+// @author       Brad Barrows
+// @icon         https://www.bradbarrows.com/favicon.ico
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+let allHBs = [];
+let currentHB = 0;
+
+const downloadCurrentDLLink = () => {
+    $("span:contains('Download')").each((index, dlb) => {
+        dlb.click();
+    });
+}
+
+const downloadFromHB = () => {
+    if (currentHB > allHBs) {
+        throw new Error("Either we downloaded all photos or none of the little hamburger buttons with the download links inside were found");
+    }
+
+    const chb = allHBs[currentHB];
+
+    chb.click();
+    setTimeout(downloadCurrentDLLink, 200);
+
+    currentHB += 1;
+    setTimeout(downloadFromHB, 1000);
+}
+
+const iterateOverAllHamburgers = () => {
+    let x = 1;
+    $("[aria-label='Edit']").each((index, hb) => {
+        allHBs.push(hb);
+    });
+
+    //setInterval(downloadFromHB, 700);
+    setTimeout(downloadFromHB, 700);
+};
+(function() {
+    'use strict';
+    // alert('hy');
+    // Your code here...
+    // $$("[aria-label='Edit']")
+    // $("span:contains('Download')")
+
+    setTimeout(iterateOverAllHamburgers, 3000);
+
+})();
